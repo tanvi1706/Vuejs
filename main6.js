8a3692ae1a85f2214df2af587d4747b983476457
tanvi <tkp190001@utdallas.edu>
2021-06-11 15:24:48 -0500

    text

--- main6.js
+++ main6.js
@@ -1,8 +1,20 @@
 var app = new Vue({
     el: "#app",
+    data: {
+        first_name: "Tanvi",
+        last_name: "Patil"
+    },
     computed: {
         getRandomComputed(){
             return Math.random();
+        },
+        getFullName(){
+            return `${ this.first_name } ${ this.last_name }`;
+        },
+        reverseFullName(){
+            first = this.first_name.split("").reverse().join("");
+            last = this.last_name.split("").reverse().join("");
+            return `${ first } ${ last }`
         }
     },
     methods: {
